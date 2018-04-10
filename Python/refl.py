42c175af939cc106c8c6ee11321bf1f69300b28a
Steve Turley <rsteventurley@gmail.com>
2018-04-10 13:25:07 -0400

    Added matrix reflectance and transmission routine in Python refl.m

--- Python/refl.py
+++ Python/refl.py
@@ -8,6 +8,75 @@ import math
 import numpy as np
 import scipy.interpolate as intp
 
+def matR(n, t, thetad, lam, sigma):
+    """Reflectance and transmittance form a multilayer mirror
+    
+    Parameters
+    ----------
+    n : array of numbers
+        index of refraction of the various layers, starting
+        with vacuum.
+    t : array of numbers
+        thickness of the various layers, starting with vacuum.
+        The thickness of the vacuum and substrate should be 0.
+    thetad: number
+        incident angle in degrees
+    sigma: array of numbers
+        roughness at each interace, starting with vacumm/top layer
+    
+    Returns
+    -------
+    tuple with reflectance and transmittance
+    
+    Example
+    -------
+    >>> matR(n, t, 45, 30.4, sigma)
+    """
+    ky=2*np.pi*np.cos(thetad*np.pi/180)/lam
+    ki = lambda ni : 2*np.pi*ni/lam
+    kzi = lambda ni : np.sqrt(ki(ni)**2-ky**2)
+    Ci = lambda ni, di : np.exp(np.complex(0, kzi(ni))*di/2)
+    def rmats(n1, n2, d1, d2):
+        kz1=kzi(n1)
+        kz2=kzi(n2)
+        fs12=(kz1-kz2)/(kz1+kz2)
+        gs12=2*kz1/(kz1+kz2)
+        fs21=-fs12
+        gs21=2*kz2/(kz1+kz2)
+        C1=Ci(n1,d1)
+        C2=Ci(n2,d2)
+        return np.array([[gs21*C1*C2-fs21*fs12*C1*C2/gs12,
+                          fs12*C1/(gs12*C2)],
+                        [-fs21*C2/(gs12*C1),
+                         1/(gs12*C1*C2)]])
+    def rmatp(n1, n2, d1, d2):
+        kz1=kzi(n1)
+        kz2=kzi(n2)
+        fp12=(n2**2*kz1-n1**2*kz2)/(n1**2*kz2+n2**2*kz1)
+        gp12=2*n1*n2*kz1/(n1**2*kz2+n2**2*kz1)
+        fp21=-fp12
+        gp21=2*n1*n2*kz2/(n1**2*kz2+n2**2*kz1)
+        C1=Ci(n1,d1)
+        C2=Ci(n2,d2)
+        return np.array([[gp21*C1*C2-fp21*fp12*C1*C2/gp12,
+                          fp12*C1/(gp12*C2)],
+                        [-fp12*C2/(gp12*C1),
+                         1/(gp12*C1*C2)]])
+    A=np.identity(2)
+    B=A
+    for i in range(n.size-1,1,-1):
+        A=np.matmul(rmats(n[i], n[i-1], t[i], t[i-1]),A)
+        B=np.matmul(rmatp(n[i], n[i-1], t[i], t[i-1]),B)
+    ts=1/A[1,1]
+    rs=ts*A[0,1]
+    tp=1/B[1,1]
+    rp=tp*B[0,1]
+    percentS=fracs(lam)
+    r=percentS*np.abs(rs)**2+(1-percentS)*np.abs(rp)**2
+    # This assumes starting and ending materials are the same
+    t=percentS*np.abs(ts)**2+(1-percentS)*np.abs(tp)**2
+    return (r,t)
+
 def Parratt(n1, n2, thetad):
     """Reflectance from a multilayer mirror
     
