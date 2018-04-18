29fd5245a1253225973b2371f98783d11c9b3f38
Steve Turley <turley@byu.edu>
2018-04-18 13:17:42 -0400

    added proportionally weighted fit

--- Python/weights.py
+++ Python/weights.py
@@ -7,7 +7,7 @@ Variable weights on reflectance fits
 @author: rturley
 """
 
-from refl import matR, Index
+from refl import Parratt, Index
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
@@ -26,28 +26,58 @@ alf3ndx = AlF3Index.at(wl)
 n=np.array([1+0j, alf3ndx, alndx, sio2ndx, sindx])
 t=np.array([0, 18, 50, 1.6, 0])
 thr = np.linspace(0.5,80,160)
-refl=[matR(n,t,thetad, wl, 0.0)[0] for thetad in thr]
+rfl=[Parratt(n,t,thetad, wl) for thetad in thr]
 # Add noise
-refn = refl*(1+0.05*np.random.randn(np.size(thr)))+1e-4*np.random.randn(np.size(thr))
+sigmap = 0.05 # proportiional noise
+sigmac = 1e-4 # constant noise
+refn = (rfl*(1+sigmap*np.random.randn(np.size(thr)))+
+        sigmac*np.random.randn(np.size(thr)))
 
 p0 = np.array([alf3ndx.real, alf3ndx.imag, 18])
 def f(thr, n, k, t):
     ndx = np.array([1+0j, n+k*1j, alndx, sio2ndx, sindx])
     th = np.array([0, t, 50, 1.6, 0])
-    return [matR(ndx ,th, thetad, wl, 0.0)[0] for thetad in thr]
+    return [Parratt(ndx ,th, thetad, wl) for thetad in thr]
 
+def print_fit(label, popt, pcov):
+    print(label)
+    print("  real(n)   = {0:7.4f} +/- {1:.5f}".format(
+            popt[0],np.sqrt(pcov[0,0])))
+    print("  imag(n)   = {0:7.4f} +/- {1:.5f}".format(
+            popt[1],np.sqrt(pcov[1,1])))
+    print("  thickness = {0:7.4f} +/- {1:.5f}".format(
+            popt[2],np.sqrt(pcov[2,2])))
+        
 sigma = np.ones(np.size(thr))
 popt, pcov = curve_fit(f, thr, refn, p0, sigma, absolute_sigma=False)
-print(popt)
+print(pcov[0,0])
+print_fit('exact', np.array([alf3ndx.real, alf3ndx.imag, 18]),
+          np.zeros([3,3]))
+print_fit('unweighted fits', popt, pcov)
 ndf = np.array([1+0j, popt[0]+popt[1]*1j, alndx, sio2ndx, sindx])
 thf = np.array([0, popt[2], 50, 1.6, 0])
-rfit = [matR(ndf ,thf, thetad, wl, 0.0)[0] for thetad in thr]
+rfit = [Parratt(ndf ,thf, thetad, wl) for thetad in thr]
 
 plt.close('all')
 plt.figure()
-plt.semilogy(thr, refn, '.', thr, rfit, '-', thr, refl, '--')
+plt.semilogy(thr, refn, '.', thr, rfit, '-', thr, rfl, '--')
 plt.legend(['data','fit','exact'])
-plt.title('Fit for AlF3/Al/SiO2/Si with Noise')
+plt.title('Unweighted Fit for AlF3/Al/SiO2/Si with Noise')
+plt.xlabel('angle, degrees')
+plt.ylabel('reflectance')
+
+# Weighted fit
+sigma=sigmap*np.array(rfl)
+popt, pcov = curve_fit(f, thr, refn, p0, sigma, absolute_sigma=True)
+print_fit('prop weighted fits', popt, pcov)
+ndf = np.array([1+0j, popt[0]+popt[1]*1j, alndx, sio2ndx, sindx])
+thf = np.array([0, popt[2], 50, 1.6, 0])
+rfit = [Parratt(ndf ,thf, thetad, wl) for thetad in thr]
+
+plt.figure()
+plt.semilogy(thr, refn, '.', thr, rfit, '-', thr, rfl, '--')
+plt.legend(['data','fit','exact'])
+plt.title('Prop Weighted Fit for AlF3/Al/SiO2/Si with Noise')
 plt.xlabel('angle, degrees')
 plt.ylabel('reflectance')
 plt.show()
