994f41f55bd2fed8b9481c5f498c410dd1e46dd8
Steve Turley <turley@byu.edu>
2018-09-07 09:55:55 -0400

    Notebook on fitting with added examples. Also notebook for data comparison.

--- Julia/Run.jl
+++ Julia/Run.jl
@@ -1,4 +1,5 @@
 using DelimitedFiles
+import DataFrames
 
 struct Run
 	x::Array{Float64,1}
@@ -6,13 +7,35 @@ struct Run
 	m3::Array{Float64,1}
 	beam::Array{Float64,1}
 	npts::Int
+	data::DataFrames.DataFrame
 end
 
+"""
+    Run(lf, run_number)
+
+# Parameters
+* `lf`: LogFile with log file data
+* `run_number`: Int run number
+"""
 function Run(lf ::LogFile, run_number :: Int)
 	fname = joinpath(dataDir,filename(lf,run_number))
 	data = readdlm(fname, skipstart=1)
 	gfact = 10^gain(lf, run_number)
 	sr = data[:,2]/gfact
 	sm3 = data[:,3]/gfact
-	Run(data[:,1],sr,sm3,data[:,4],size(data)[1])
+	Run(data[:,1],sr,sm3,data[:,4],size(data)[1],lf.data[run_number,:])
+end
+
+"""
+    gain(r)
+
+# Parameters
+* `r`: Run
+"""
+function gain(r::Run)
+	r.data[Symbol("gain/count1")][1]
+end
+
+function wavelength(r::Run)
+	r.data[:mono][1]
 end
